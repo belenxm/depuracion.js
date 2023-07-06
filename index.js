248e3e8d778bcf9536caa16e6197bf57103f1570
fer.belen <belenxmorales@gmail.com>
2023-07-06 17:42:08 -0300

    depuracion

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-class Estudiante {
-    constructor(nombre){
-    this.nombre = nombre;
-    this.asignaturas = ['Javascript', 'HTML', 'CSS'];
-    
+function fibonacciSequence(num) {
+  var sequence = [];
+
+  if (num === 0) {
+    return sequence;
+  }
+
+  sequence.push(0);
+
+  if (num === 1) {
+    return sequence;
+  }
+
+  sequence.push(1);
+
+  for (var i = 2; i < num; i++) {
+    var nextNumber = sequence[i - 1] + sequence[i - 2];
+    sequence.push(nextNumber);
+  }
+
+  return sequence;
 }
-
-obtenDatos(){
-    return{
-        nombre : this.nombre,
-        asignaturas:this.asignaturas
-    };
-}
-}
-
-
-const estudiante = new Estudiante('Nombre del estudiante');
-const datosEstudiante = estudiante.obtenDatos();
-console.log(datosEstudiante);
-
-
-
-
-
